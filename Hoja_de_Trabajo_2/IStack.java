4b938666f93e612865e04c5dbc666ff11f1d18c4
JAVIERCLO <cha23132@uvg.edu.gt>
2025-01-25 18:32:55 -0600

    Diagrama UML y cambio sugerido a la interfaz

--- Hoja_de_Trabajo_2/IStack.java
+++ Hoja_de_Trabajo_2/IStack.java
@@ -3,5 +3,9 @@ public interface IStack {
     void push(int value);
 	int pop();
 	int operation(char operator, int value1, int value2);
+	/*
+	 * Metodos sugeridos
+	 * Boolean isEmpty();
+	 */
 
 }
