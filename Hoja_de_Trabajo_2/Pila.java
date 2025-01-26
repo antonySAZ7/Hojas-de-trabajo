85f154d741d12a2026039bd930875c68d6f41c52
antonySAZ7 <dantony.saz7@gmail.com>
2025-01-25 20:58:47 -0600

    Clase pila que es la clase modelo que maneja las funciones de lo que deberia hacer la pila

---
+++ Hoja_de_Trabajo_2/Pila.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pila implements  IStack {
+
+    private  List<Integer> elementos ;
+
+
+    public Pila(){
+        this.elementos = new ArrayList<>();
+
+    }
+
+    public void push(int value){
+        elementos.add(value);
+
+    }
+
+    public int pop(){
+        if(elementos.isEmpty()){
+            throw new IllegalStateException("La pila está vacía.");
+        }
+        return  elementos.remove(elementos.size()-1);
+
+    }
+
+    public int operation(char operator, int value1, int value2){
+        switch(operator){
+            case '+': return value1 + value2;
+            case '-': return value1 - value2;
+            case '*': return value1 * value2;
+            case '/': 
+            if(value2 ==0){
+            throw new ArithmeticException("No se puede dividir por cero");
+            }
+            return value1/value2;
+            case '%': return value1%value2;
+            default: throw new IllegalArgumentException("Operador no válido: " + operator);
+        }
+    }
+}
